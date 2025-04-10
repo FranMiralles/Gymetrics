6812b5d04b9a9aa90db95a73456f0b55b0c74ae7
Fran Miralles <franmiralles03@gmail.com>
2025-04-10 15:30:40 +0200

    try catch users

--- src/pages/api/users.ts
+++ src/pages/api/users.ts
@@ -1,10 +1,14 @@
-// pages/api/users.ts
 import { PrismaClient } from '@prisma/client'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 const prisma = new PrismaClient()
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const users = await prisma.user.findMany()
-  res.status(200).json(users)
+  try {
+    const users = await prisma.user.findMany()
+    res.status(200).json(users)
+  } catch (error) {
+    console.error('Error fetching users:', error) // Registra el error completo
+    res.status(500).json({ message: 'Error interno del servidor' })
+  }
 }
